9b787c1f89f310f8dcedc27c7f4355710ecafc0d
abhiaccent <abhi.shrivastava@accenture.com>
2026-01-28 05:09:16 +0000

    d

--- octofit-tracker/frontend/src/components/Navigation.js
+++ octofit-tracker/frontend/src/components/Navigation.js
@@ -5,7 +5,10 @@ import { Link } from 'react-router-dom';
 const Navigation = () => (
   <Navbar bg="dark" variant="dark" expand="lg">
     <Container>
-      <Navbar.Brand as={Link} to="/">Octofit Tracker</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
+        <img src={process.env.PUBLIC_URL + '/octofitapp-small.png'} alt="Octofit Logo" />
+        Octofit Tracker
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
